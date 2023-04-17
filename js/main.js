beff1583094039288fa4906ef5eb4f47c7ee2d37
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-04-17 18:05:50 +0200

    fix js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
+const cont = document.getElementById("container")
 for (let i = 0; i < 100; i++) {
     let x = (i + 1)
+    let cardColor = "purple"
     let multiploTre = (x % 3)
-    let multiploCinquie = (x % 5)
-    const cont = document.getElementById("container")
-    if (multiploTre == 0 && multiploCinquie == 0) {
-        cont.innerHTML += `<div class="card red"> FizBuz </div>`
+    let multiploCinque = (x % 5)
+    if (multiploTre == 0 && multiploCinque == 0) {
+        x = "FizBuz"
+        cardColor = "red"
     }else if (multiploTre == 0) {
-        cont.innerHTML += `<div class="card green"> Fiz </div>`
-    }else if (multiploCinquie == 0 ) {
-        cont.innerHTML += `<div class="card blue"> Buz </div>`
-    }else {
-        cont.innerHTML += `<div class="card purple"> ${x} </div>`
-    } 
+        x = "Fiz"
+        cardColor = "green"
+    }else if (multiploCinque == 0 ) {
+        x = "Buz"
+        cardColor = "blue"
+    }
+
+    cont.innerHTML += `<div class="card ${cardColor}"> ${x} </div>`
+     
 }
