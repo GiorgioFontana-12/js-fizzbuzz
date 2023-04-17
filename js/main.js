0a1820f1addc97b36a43fc1416c2f5cd69036302
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-04-17 17:33:46 +0200

    end project

--- js/main.js
+++ js/main.js
@@ -2,12 +2,14 @@ for (let i = 0; i < 100; i++) {
     let x = (i + 1)
     let multiploTre = (x % 3)
     let multiploCinquie = (x % 5)
-    if (multiploTre == 0) {
-        x = "Fiz"
+    const cont = document.getElementById("container")
+    if (multiploTre == 0 && multiploCinquie == 0) {
+        cont.innerHTML += `<div class="card red"> FizBuz </div>`
+    }else if (multiploTre == 0) {
+        cont.innerHTML += `<div class="card green"> Fiz </div>`
     }else if (multiploCinquie == 0 ) {
-        x = "Buz"
-    }else if (multiploTre == 0 && multiploCinquie == 0) {
-        x = "FizBuz"
-    }  
-    console.log (x)
+        cont.innerHTML += `<div class="card blue"> Buz </div>`
+    }else {
+        cont.innerHTML += `<div class="card purple"> ${x} </div>`
+    } 
 }
